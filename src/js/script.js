02a2396666ecebfd05302723f18587f90c34df68
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-05-17 18:11:12 +0100

    add responsive menu functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+document.getElementById("hamburger").onclick = function () {
+  this.classList.toggle("is-active");
+};
+
+const button = document.querySelector("#hamburger");
+
+button.addEventListener("click", (e) => {
+  document.querySelector(".show-nav").classList.toggle("header__nav");
+});
