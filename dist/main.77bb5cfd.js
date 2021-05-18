7a42a464ca000b5afe46a29bba8a1fed9d2ab8f9
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-05-18 18:48:12 +0100

    add to dist

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\icon-arrow.svg":[["icon-arrow.56d7c493.svg","images/icon-arrow.svg"],"images/icon-arrow.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60803" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50272" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
